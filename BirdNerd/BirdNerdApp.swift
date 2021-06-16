1ad26b26fe885703e0851114bb04592484acbd69
Scarlett Luo <jscarlett.luo@berkeley.edu>
2021-06-16 11:25:20 -0700

    Initial Commit

---
+++ BirdNerd/BirdNerdApp.swift
@@ -0,0 +1,17 @@
+//
+//  BirdNerdApp.swift
+//  BirdNerd
+//
+//  Created by Scarlett Luo on 6/16/21.
+//
+
+import SwiftUI
+
+@main
+struct BirdNerdApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BirdNerd/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  BirdNerd
+//
+//  Created by Scarlett Luo on 6/16/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
